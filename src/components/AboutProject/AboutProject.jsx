6f2cb9d6443e7f634ee15692fcb95e0e1a119f87
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-13 11:47:29 +0400

    feat: add the About Project's layout

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -4,21 +4,23 @@ import './AboutProject.css';
 function AboutProject() {
   return (
     <section className="about-project">
-      <h2 className="about-project__title about-project__title_location_top">О&nbsp;проекте</h2>
-      <h3 className="about-project__title about-project__title_location_middle">
-        Дипломный проект включал 5&nbsp;этапов
-      </h3>
-      <p className="about-project__text">
-        Составление плана, работу над бэкендом, вёрстку, добавление функциональности
-        и&nbsp;финальные доработки.
-      </p>
-      <h3 className="about-project__title about-project__title_location_bottom">
-        На&nbsp;выполнение диплома ушло 5&nbsp;недель
-      </h3>
-      <p className="about-project__text">
-        У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы
-        успешно защититься.
-      </p>
+      <h2 className="about-project__title">О&nbsp;проекте</h2>
+      <div className="about-project__text-section">
+        <div className="about-project__text-block">
+          <h3 className="about-project__subtitle ">Дипломный проект включал 5&nbsp;этапов</h3>
+          <p className="about-project__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности
+            и&nbsp;финальные доработки.
+          </p>
+        </div>
+        <div className="about-project__text-block">
+          <h3 className="about-project__subtitle ">На&nbsp;выполнение диплома ушло 5&nbsp;недель</h3>
+          <p className="about-project__text">
+            У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать,
+            чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
       <div className="about-project__timeline">
         <div className="about-project__timeline-box about-project__timeline-box_type_backend">
           <p className="about-project__timeline-term about-project__timeline-term_type_backend">
