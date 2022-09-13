32ec2744cad5452ccf54b091e8b91f10b9fb2705
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-13 09:14:14 +0400

    chore: add NBSP

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -18,7 +18,7 @@ function Promo() {
       </picture>
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
       <p className="promo__subtitle">
-        Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        Листайте ниже, чтобы узнать больше про этот проект и&nbsp;его создателя.
       </p>
       <Button
         isSubmitButton={false}
