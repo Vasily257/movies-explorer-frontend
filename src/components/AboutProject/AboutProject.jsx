9d2733f3adb039aea7cc8d7dcaa7a5c18389f853
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-13 09:54:01 +0400

    fix: fix About Project Markup, delete Timeline

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import Timeline from '../Timeline/Timeline';
 import './AboutProject.css';
 
 function AboutProject() {
   return (
     <section className="about-project">
-      <h2 className="about-project__title">О&nbsp;проекте</h2>
-      <h3 className="about-project__subtitle">Дипломный проект включал 5&nbsp;этапов</h3>
+      <h2 className="about-project__title about-project__title_location_top">О&nbsp;проекте</h2>
+      <h3 className="about-project__title about-project__title_location_middle">
+        Дипломный проект включал 5&nbsp;этапов
+      </h3>
       <p className="about-project__text">
         Составление плана, работу над бэкендом, вёрстку, добавление функциональности
         и&nbsp;финальные доработки.
       </p>
-      <h3 className="about-project__subtitle">На&nbsp;выполнение диплома ушло 5&nbsp;недель</h3>
+      <h3 className="about-project__title about-project__title_location_bottom">
+        На&nbsp;выполнение диплома ушло 5&nbsp;недель
+      </h3>
       <p className="about-project__text">
         У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы
         успешно защититься.
       </p>
-      <Timeline />
+      <div className="about-project__timeline">
+        <div className="about-project__timeline-box">
+          <p className="about-project__timeline-term about-project__timeline-term_type_backend">
+            1&nbsp;неделя
+          </p>
+          <p className="about-project__timeline-description">Back-end</p>
+        </div>
+        <div className="about-project__timeline-box">
+          <p className="about-project__timeline-term about-project__timeline-term_type_frontend">
+            4&nbsp;недели
+          </p>
+          <p className="about-project__timeline-description">Front-end</p>
+        </div>
+      </div>
     </section>
   );
 }
