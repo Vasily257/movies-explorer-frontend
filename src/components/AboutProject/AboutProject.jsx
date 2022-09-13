2cc667c9557698f7c1a73a704ee6e49580cd5893
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-13 09:10:55 +0400

    feat: add a markup of About Project and Timeline

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Timeline from '../Timeline/Timeline';
 import './AboutProject.css';
 
 function AboutProject() {
-  return <section className="about-project" />;
+  return (
+    <section className="about-project">
+      <h2 className="about-project__title">О&nbsp;проекте</h2>
+      <h3 className="about-project__subtitle">Дипломный проект включал 5&nbsp;этапов</h3>
+      <p className="about-project__text">
+        Составление плана, работу над бэкендом, вёрстку, добавление функциональности
+        и&nbsp;финальные доработки.
+      </p>
+      <h3 className="about-project__subtitle">На&nbsp;выполнение диплома ушло 5&nbsp;недель</h3>
+      <p className="about-project__text">
+        У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы
+        успешно защититься.
+      </p>
+      <Timeline />
+    </section>
+  );
 }
 
 export default AboutProject;
