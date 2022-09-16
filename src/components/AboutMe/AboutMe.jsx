a0f3fc74cc11a1c9e7492729744e4521467991bf
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-16 14:37:54 +0400

    feat: add About Me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './AboutMe.css';
+import photo from '../../images/about-me-photo.jpg';
+import CustomLink from '../CustomLink/CustomLink';
 
 function AboutMe() {
-  return <section className="about-me" />;
+  return (
+    <section className="about-me">
+      <h2 className="about-me__title">Студент</h2>
+      <div className="about-me__inner">
+        <img className="about-me__photo" src={photo} alt="Фотография разработчика" />
+        <h3 className="about-me__name ">Василий</h3>
+        <p className="about-me__profession">Фронтенд-разработчик, 29&nbsp;лет</p>
+        <p className="about-me__text">
+          Родился в&nbsp;Самаре, живу примерно тут&nbsp;же. Работал по&nbsp;специальности
+          на&nbsp;ТЭЦ, но&nbsp;через пару лет понял, что не&nbsp;моё. Хотел стать бизнес-тренером,
+          но&nbsp;вместо этого увлекся разработкой электронных курсов. Затем осознал, что мне
+          нравится вёрстка, поэтому решил изучать веб-разработку. Есть жена и&nbsp;две кошки:
+          Кис-Кис и&nbsp;Мур-Мур.
+        </p>
+        <CustomLink className="about-me__link" target="_blank" path="http://github.com/Vasily257">
+          Github
+        </CustomLink>
+      </div>
+    </section>
+  );
 }
 
 export default AboutMe;
