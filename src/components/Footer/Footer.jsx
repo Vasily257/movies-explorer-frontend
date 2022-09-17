f45b24a91ac879c3ca28c34ab9c4a32aa66f54b2
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-17 11:29:44 +0400

    feat: add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 import './Footer.css';
 
 function Footer() {
-  return <footer className="footer" />;
+  return (
+    <footer className="footer">
+      <p className="footer__about">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <ul className="footer__list">
+        <li className="footer__item footer__item_type_link">
+          <CustomLink className="footer__link" target="_blank" path="https://practicum.yandex.ru/">
+            Яндекс.Практикум
+          </CustomLink>
+        </li>
+        <li className="footer__item footer__item_type_link">
+          <CustomLink
+            className="footer__link"
+            target="_blank"
+            path="https://github.com/yandex-praktikum"
+          >
+            Github
+          </CustomLink>
+        </li>
+        <li className="footer__item footer__item_type_copyright">&copy;2022</li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
