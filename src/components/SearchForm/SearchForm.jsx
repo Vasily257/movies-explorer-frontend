39f1815d529e66a80b7f41e47b1a954eb6acb768
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-18 16:23:16 +0400

    feat: add Search Form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import Button from '../Button/Button';
+
+import searchFormIcon from '../../images/search-form-icon.svg';
+
 import './SearchForm.css';
 
 function SearchForm() {
-  return <form className="search-form" />;
+  return (
+    <section className="search-form">
+      <h1 className="visually-hidden">Поиск по фильмам</h1>
+      <form className="search-form__form">
+        <p className="search-form__field">
+          <label className="search-form__label" htmlFor="movie-input">
+            <span className="visually-hidden">Фильм</span>
+            <input
+              className="search-form__input"
+              type="text"
+              name="movie"
+              id="movie-input"
+              defaultValue="Фильм"
+              minLength="2"
+              maxLength="40"
+              required
+            />
+          </label>
+          <Button className="search-form__submit-button" onClick={{}} isSubmitButton>
+            <img
+              className="search-form__submit-button-image"
+              src={searchFormIcon}
+              alt="Найти фильм"
+            />
+          </Button>
+        </p>
+        <div className="search-form__shorts-wrapper">
+          <Button
+            className="search-form__shorts-button"
+            onClick={{}}
+            ariaLabel="Искать только короткометражные фильмы"
+          />
+          <span className="search-form__shorts-text">Короткометражки</span>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
