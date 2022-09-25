36eacfe0e0ed6c93a20e4555c1b6a1c6d0bc738f
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-25 19:29:41 +0400

    fix: move the search button to its place

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,10 @@
 import { React, useState } from 'react';
 import Button from '../Button/Button';
+import Input from '../Input/Input';
 
 import searchFormIcon from '../../images/search-form-icon.svg';
 
 import './SearchForm.css';
-import Input from '../Input/Input';
 
 function SearchForm() {
   const [isShortsMovies, setIsShortsMovies] = useState(true);
@@ -13,25 +13,23 @@ function SearchForm() {
     <section className="search-form">
       <h1 className="visually-hidden">Поиск по фильмам</h1>
       <form className="search-form__form">
-        <p className="search-form__field">
-          <Input
-            inputClassName="search-form__input"
-            type="text"
-            name="movie"
-            id="movie-input"
-            onChange={() => {}}
-            placeholder="Фильм"
-            labelText="Фильм"
-            required
+        <Input
+          inputClassName="search-form__input"
+          type="text"
+          name="movie"
+          id="movie-input"
+          onChange={() => {}}
+          placeholder="Фильм"
+          labelText="Фильм"
+          required
+        />
+        <Button className="search-form__submit-button" onClick={() => {}} isSubmitButton>
+          <img
+            className="search-form__submit-button-image"
+            src={searchFormIcon}
+            alt="Найти фильм"
           />
-          <Button className="search-form__submit-button" onClick={() => {}} isSubmitButton>
-            <img
-              className="search-form__submit-button-image"
-              src={searchFormIcon}
-              alt="Найти фильм"
-            />
-          </Button>
-        </p>
+        </Button>
         <div className="search-form__shorts-wrapper">
           <Button
             className={`search-form__shorts-button ${
