0a9e8cfbe242e4f70aa4dd4fdb40a0bb790fd47a
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-25 16:00:50 +0400

    chore: remove unnecessary components

--- src/components/LoginForm/LoginForm.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './LoginForm.css';
-
-function LoginForm() {
-  return <form className="login-form" />;
-}
-
-export default LoginForm;
--- src/components/RegisterForm/RegisterForm.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './RegisterForm.css';
-
-function RegisterForm() {
-  return <form className="register-form" />;
-}
-
-export default RegisterForm;
