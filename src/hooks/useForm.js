02f7c2396e268708fad40cd0ea8d764c41e2b8cc
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-10-01 17:55:35 +0400

    feat: add use form hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,33 @@
+import { useState, useCallback } from 'react';
+
+export function useForm() {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value, validationMessage } = event.target;
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: validationMessage });
+    setIsValid(event.target.closest('form').checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid],
+  );
+
+  return {
+    values,
+    handleChange,
+    errors,
+    isValid,
+    resetForm,
+  };
+}
+
+export default useForm;
