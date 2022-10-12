e53b2eee90349a7f8ec8cf11b2d1b9c57de0f40a
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-10-12 07:26:34 +0400

    feat: add creating card API

--- src/utils/scripts/MainApi.js
+++ src/utils/scripts/MainApi.js
@@ -63,6 +63,42 @@ async function updateProfile({ name, email, currentEmail }) {
   return handleResponse(response);
 }
 
+async function createCard({
+  country,
+  director,
+  duration,
+  year,
+  description,
+  image,
+  trailerLink,
+  nameRU,
+  nameEN,
+  thumbnail,
+  movieId,
+  owner,
+}) {
+  const response = await fetch(`${BASE_URL.MOVIES_EXPLORER}/movies`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+      owner,
+    }),
+  });
+
+  return handleResponse(response);
+}
+
 export {
-  setToken, register, login, getContent, updateProfile,
+  setToken, register, login, getContent, updateProfile, createCard,
 };
